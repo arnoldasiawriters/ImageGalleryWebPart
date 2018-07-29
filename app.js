db3dd53760e246b242dc575214f3aba7698f2a6c
arnoldasiawriters <manarnoldi@gmail.com>
2018-07-29 20:43:15 +0300

    Second commit

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-ECHO is on.
+(function () {
+    'use strict';
+
+    angular
+        .module('ImageGalleryApp', ['ImageGalleryUtils'])
+        .controller('ImageGalleryController', ImageGalleryController)
+        .constant('currListName', 'ImageGalleryPicLib');
+
+    ImageGalleryController.$inject = ['ImageGalleryService', 'currListName'];
+    function ImageGalleryController(ImageGalleryService, currListName) {
+        var imageGallery = this;
+
+        var folders_promise = ImageGalleryService.get_folder_names();
+
+        folders_promise
+            .then(function (data) {
+                imageGallery.folder_names = data.data.d;
+                console.log(currListName);
+
+                angular.forEach(imageGallery.folder_names, function (v, k) {
+                    var folder_nm = v.parent.substring(1);
+                    if (folder_nm == currListName) {
+                        console.log(v);
+                    }
+                });
+            })
+            .catch(function (error) {
+                console.log(error.message);
+            });
+    }
+})();
--- services.js
+++ services.js
@@ -1,1 +1,33 @@
-ECHO is on.
+(function () {
+    'use strict';
+
+    angular
+        .module('ImageGalleryUtils', [])
+        .service('ImageGalleryService', ImageGalleryService)
+        .directive('listItems', ListItems);
+
+    ImageGalleryService.$inject = ['$http'];
+    function ImageGalleryService($http) {
+        var service = this;
+
+        service.get_folder_names = function () {
+            var response = $http({
+                method: 'GET',
+                url: 'folder_names.json'
+            });
+            return response;
+        };
+    }
+
+    function ListItems() {
+        var ddo = {
+            restrict:'E',
+            templateUrl: "list_items.tpl.html",
+            scope: {
+                folders: "=folders"
+            },
+            
+        };
+        return ddo;
+    }
+})();
